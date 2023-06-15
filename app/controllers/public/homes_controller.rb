bdc4db786d4090d8e2f1de22017b5ddb4fd38f5f
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-06-15 12:17:56 +0000

    [Update]トップとアバウト

---
+++ app/controllers/public/homes_controller.rb
@@ -0,0 +1,10 @@
+class Public::HomesController < ApplicationController
+  
+  def top
+  
+  end
+  
+  def about
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+  
+  scope module: :public do
+    root :to => "homes#top"
+    get "/about" => "homes#about"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
