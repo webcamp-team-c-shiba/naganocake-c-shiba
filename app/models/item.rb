1984a6ed6bde122f4e881906d805e98d45e995fa
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-06-16 13:03:22 +0000

    [Add]商品一覧・商品詳細ページ追加

---
+++ app/models/item.rb
@@ -0,0 +1,16 @@
+class Item < ApplicationRecord
+  has_one_attached :image
+  
+  # 消費税
+  def with_tax_price
+    (price * 1.1).floor
+  end
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] , controllers: {
-    sessions: "admin/sessions" 
+    sessions: "admin/sessions"
   }
-  
+
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+
+  root to:'public/homes#top'
+  get 'about' => 'public/homes#about'
+  get 'admin' => 'admin/homes#top'
+
+  scope module: :public do
+    resources :items,only: [:index,:show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
