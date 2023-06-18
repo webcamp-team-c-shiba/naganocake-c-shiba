afa919284ff8832ab4ae4e6d70c14982ea580666
EC2 Default User <132253022+Asakaku@users.noreply.github.com>
2023-06-18 12:55:43 +0000

    Add homes#top

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'homes/top'
+    get '/' => 'homes#top'
   end
   devise_for :admin, skip: [:registrations, :passwords] , controllers: {
     sessions: "admin/sessions" 
