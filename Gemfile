3745d0c851ab7a27400859ea6bd481b8a4bf403e
EC2 Default User <132253022+Asakaku@users.noreply.github.com>
2023-06-18 12:04:26 +0000

    kaminari install

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 
 gem 'kaminari','~> 1.2.1'
+
 gem 'bootstrap5-kaminari-views'
 
 gem 'pry-byebug', group: :development
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,7 +2,6 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-　　before_action :configure_permitted_parameters, if: :devise_controller?
   
   # GET /resource/sign_in
   # def new
@@ -19,9 +18,12 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
   
-  def destroy
-    reset_session
-    redirect_to new_admin_session_path
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+ 
+  def after_sign_out_path_for(resource)
+    root_path
   end
 
   # protected
