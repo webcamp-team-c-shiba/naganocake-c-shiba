0f2e7d7d2817b3ca35e276f8a8cce2421fed9033
EC2 Default User <132253022+Asakaku@users.noreply.github.com>
2023-06-18 12:09:35 +0000

    　[Add]admin_sessions

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,7 +2,6 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-　　before_action :configure_permitted_parameters, if: :devise_controller?
   
   # GET /resource/sign_in
   # def new
@@ -19,13 +18,15 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
   
-  def destroy
-    reset_session
-    redirect_to new_admin_session_path
+  def after_sign_in_path_for(resource)
+    admin_items_path
+  end
+ 
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
   end
 
   # protected
-　
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
