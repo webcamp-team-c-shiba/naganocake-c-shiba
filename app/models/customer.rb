6c777b152a0392c79ad689be24a1b0bb4d1622de
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-06-19 13:20:39 +0000

    [fix]image表示＋customerとのアソシエーション

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  belongs_to :cart_items
          
   validates :last_name, presence: true
   validates :first_name, presence: true
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,25 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  
+    
+  belongs_to :genre
   has_many :cart_items, dependent: :destroy
+
+  validates :genre_id, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   
-  # 消費税
-  def with_tax_price
-    (price * 1.1).floor
-  end
-  
-  def get_image
+  def get_item_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  # 消費税
+  def with_tax_price
+    (price * 1.1).floor
   end
   
 end
