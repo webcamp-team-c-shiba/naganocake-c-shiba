0661692c0be54b56e3304268b0ebc17f2822b404
EC2 Default User <132253022+Asakaku@users.noreply.github.com>
2023-06-19 10:06:28 +0000

    first commit

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,19 @@
+class Public::OrdersController < ApplicationController
+  def new
+  end
+
+  def check
+  end
+
+  def complete
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
   
+  namespace :public do
+    # Rails.application.routes.draw do
+    # get '/orders/new', to: 'orders#new'
+    # end
+    # # get 'orders/new' , to: 'orders#new'
+    # get 'orders/check' => 'orders#check'
+    # get 'orders/complete' => 'orders#complete'
+    # get 'orders/create' => 'orders#create'
+    # get 'orders/index' => 'orders#index'
+    # get 'orders/show' => 'orders#show'
+  resources :orders, only: [:new, :check, :complete, :create, :index, :show]
   resources :addresses, except: [:show]
+  end
   
   namespace :admin do
+    get '/' => 'homes#top'
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, except: [:destroy]
-    get '/' => 'homes#top'
   end
   
   devise_for :admin, skip: [:registrations, :passwords] , controllers: {
