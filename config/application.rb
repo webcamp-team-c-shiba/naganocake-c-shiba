b52a4973bdf65e2e44fc81d86ce0cccd1661a684
EC2 Default User <132253022+Asakaku@users.noreply.github.com>
2023-06-20 08:11:25 +0000

    [Add]Public_orders

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module NaganoCake
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.default_locale = :ja
   end
 end
