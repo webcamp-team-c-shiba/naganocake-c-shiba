9370498feb4deff5249adea1c5f28c6fb72b79d1
EC2 Default User <ec2-user@ip-172-31-9-250.ap-northeast-1.compute.internal>
2023-06-20 08:38:28 +0000

    [Add]退会機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   
+  root to:'public/homes#top'
+  get 'about' => 'public/homes#about'
+  
+  
   scope module: :public do
     resources :addresses, except: [:show]
+    
     resource :customers, only: [:show, :edit, :update]
     get 'customers/check' => "customers#check"
     patch 'customers/withdrawal' => "customers#withdrawal"
+    
+    resources :items, only: [:index,:show]
+    resources :cart_items, only: [:index,:create,:destroy,:update]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
   end
 
   
@@ -22,15 +31,5 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-
-  root to:'public/homes#top'
-  get 'about' => 'public/homes#about'
-
-  scope module: :public do
-    resources :items, only: [:index,:show]
-    resources :cart_items, only: [:index,:create,:destroy,:update]
-    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
-    
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
