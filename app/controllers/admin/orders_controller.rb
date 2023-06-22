a856302bd87abd2d27e9172e24e2edbfaa220b2a
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-06-22 06:58:04 +0000

    [Add]管理者注文履歴一覧追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,6 @@
 class Admin::OrdersController < ApplicationController
   def index
+    @orders = Order.all.order(created_at: :desc) 
   end
 
   def show
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,8 @@ class Order < ApplicationRecord
     
     enum payment_method: { credit_card: 0, transfer: 1 }
     enum status: { waiting_payment:0, confirmation_payment: 1, under_process: 2, waiting_shipping: 3, completed_shipping: 4 }
+    
+    def amount_sum
+        
+    end
 end
