df02ce64a7c255d417e452246b43d977ff36f48a
EC2 Default User <132253022+Asakaku@users.noreply.github.com>
2023-06-22 08:11:55 +0000

    [Add]注文詳細画面の追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,9 +3,18 @@ class Admin::OrdersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(current_customer.id)
+    @orders = Order.all
+    @selected_order = Order.find(params[:id])
   end
   
   def update
+  end
+
+  private
+  
+  def order_params
+    params.require(:order).permit(:shipping_fee, :payment, :payment_method, :postcode, :address, :name)
   end
   
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,11 @@ class Order < ApplicationRecord
     
     enum payment_method: { credit_card: 0, transfer: 1 }
     enum status: { waiting_payment:0, confirmation_payment: 1, under_process: 2, waiting_shipping: 3, completed_shipping: 4 }
+    enum status: {
+     "入金待ち":0, 
+     "入金確認":1, 
+     "製作中":2, 
+     "発送準備中":3, 
+     "発送済み":4
+  }
 end
