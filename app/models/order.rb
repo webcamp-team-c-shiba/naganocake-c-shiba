cdd1802ae5f1e2d82793cb4b7ff4e9e23c7a1ab5
EC2 Default User <132253022+Asakaku@users.noreply.github.com>
2023-06-22 12:55:01 +0000

    [Fix]注文履歴詳細画面の修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,15 +3,9 @@ class Order < ApplicationRecord
     has_many :items, through: :cart_items
     
     has_many :order_items, dependent: :destroy
-    has_many :item, through: :order_items
+    has_many :items, through: :order_items
     
     enum payment_method: { credit_card: 0, transfer: 1 }
-    enum status: { waiting_payment:0, confirmation_payment: 1, under_process: 2, waiting_shipping: 3, completed_shipping: 4 }
-    enum status: {
-     "入金待ち":0, 
-     "入金確認":1, 
-     "製作中":2, 
-     "発送準備中":3, 
-     "発送済み":4
-  }
+    enum order_status: { "入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送済":4 }
+    enum product_status: { "着手不可": 0, "製作待ち": 1, "製作": 2, "製作完了": 3 }
 end
