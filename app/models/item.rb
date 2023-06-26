2c170f5ca0f2f270f0cdbc280ce9740ed89f4d42
EC2 Default User <ec2-user@ip-172-31-9-250.ap-northeast-1.compute.internal>
2023-06-26 07:29:49 +0000

    [Fix]商品画像でimageファイルのみバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
 
   validates :genre_id, presence: true
   validates :image, presence: true
+  validates_format_of :image, with: /\A.*\.(png|jpg|jpeg|gif)\z/i, message: "(PNG, JPG, JPEG, GIF)ファイルを選択してください"
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
